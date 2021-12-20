cf1697da831601c79da6e7253cae6d0ac4771d46
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2021-12-20 18:08:29 -0300

    inserindo setAutoCommit

--- src/br/com/loja/virtual/db/test/TestaInsercaoComParametro.java
+++ src/br/com/loja/virtual/db/test/TestaInsercaoComParametro.java
@@ -12,26 +12,33 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "TV";
-		String descricao = "TV - 70 POLEGADAS";
-		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection conn = connectionFactory.recuperarConexao();
-		
-		
+		conn.setAutoCommit(false);
 
-		PreparedStatement stm = conn.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-		
+		PreparedStatement stm = conn.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		adicionarVariavel("MP4", "MP4 AZUL", stm);
+		adicionarVariavel("Radio", "Radio AM/FM", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 		
+		if(nome.equals("Radio")) {
+			throw new RuntimeException("Não foi possivel adicionar o PRODUTO");
+		}
+
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
 		while (rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O ID criado foi: " + id);
-	
+
 		}
+		rst.close();
 	}
 }
